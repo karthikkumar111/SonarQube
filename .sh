77d8c5611e228ce6dd0b9efbd7c9d9a7859ed80f
karthikkumar111 <131590180+karthikkumar111@users.noreply.github.com>
2025-07-07 08:59:53 +0530

    sonarqube.sh

---
+++ .sh
@@ -0,0 +1,24 @@
+#install java
+amazon-linux-extras install java-openjdk11 -y
+sudo cd /opt
+#sonarqube link address
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.5.90363.zip
+#unzip
+unzip sonarqube-9.9.5.90363.zip
+#rename
+mv ssonarqube-9.9.5.90363 sonar
+#useradd sonar
+useradd sonar
+#change owners
+chown sonar:sonar filename  -R
+#give full permissions
+chmod 777 filename -R
+su - sonar
+#change directory to opt folder
+cd /opt
+ll
+cd sonar
+cd bin
+cd linux
+./sonar.sh start
+./sonar status
